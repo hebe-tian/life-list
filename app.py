fa485dd41e24e2783bf96e31c69f27a19d90c164
hebe_tien <linleil@126.com>
2023-04-18 20:37:20 +0800

    delete index.html add 4 type pages, add aboutme page ,define search method

--- app.py
+++ app.py
@@ -26,11 +26,18 @@ class User(db.Model):
 
 # tag 枚举，后续定义
 class Item(db.Model):
+    # 主键 id
     id = db.Column(db.Integer, primary_key=True)
+    # 名称
     title = db.Column(db.String(60))
+    # 链接
     url = db.Column(db.String(40))
+    # 标签 MOVIE/BOOK/STORE/PLACE
     tag = db.Column(db.String(20))
+    # 热度
     star = db.Column(db.Integer)
+    # 状态 WANT/USED
+    state = db.Column(db.Integer)
 
 
 @app.cli.command()
@@ -42,11 +49,52 @@ def initdb(drop):
     click.echo('DB Ready.')
 
 
+def search(tag):
+    content = Item.query.filter_by(tag=tag).all()
+    print(content)
+    return content
+
+
 @app.route('/')
-def hello():
+def about_me():
+    return render_template('me.html')
+
+
+@app.route('/movie')
+def search_movie():
+    movies = search('MOVIE')
+    return render_template('movie.html', movies=movies)
+
+
+@app.route('/book')
+def search_book():
+    books = search('BOOK')
+    return render_template('book.html', books=books)
+
+
+@app.route('/store')
+def search_store():
+    stores = search('STORE')
+    return render_template('store.html', stores=stores)
+
+
+@app.route('/place')
+def search_place():
+    places = search('PLACE')
+    return render_template('place.html', places=places)
+
+
+# 上下文处理器，每个模版都会调用return_user
+@app.context_processor
+def return_user():
     user = User.query.first()
-    movies = Item.query.filter_by(tag='MOVIE').all()
-    return render_template('index.html', user=user, movies=movies)
+    return dict(user=user)
+
+
+# 404页面
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 # create fake data
@@ -58,14 +106,20 @@ def forge():
     # 全局的两个变量移动到这个函数内
     name = 'DiuDiu'
     movies = [
-        {'title': 'My Neighbor Totoro', 'url': 'https://movie.douban.com/subject/1291560/', 'tag': 'MOVIE'},
-        {'title': 'Dead Poets Society', 'url': 'https://movie.douban.com/subject/1291548/', 'tag': 'MOVIE'}
+        {'title': 'My Neighbor Totoro', 'url': 'https://movie.douban.com/subject/1291560/', 'tag': 'MOVIE',
+         'state': 'USED'},
+        {'title': 'Dead Poets Society', 'url': 'https://movie.douban.com/subject/1291548/', 'tag': 'BOOK',
+         'state': 'WANT'},
+        {'title': 'My Neighbor Totoro', 'url': 'https://movie.douban.com/subject/1291560/', 'tag': 'PLACE',
+         'state': 'USED'},
+        {'title': 'Dead Poets Society', 'url': 'https://movie.douban.com/subject/1291548/', 'tag': 'STORE',
+         'state': 'WANT'}
     ]
 
     user = User(name=name)
     db.session.add(user)
     for m in movies:
-        movie = Item(title=m['title'], url=m['url'], tag=m['tag'])
+        movie = Item(title=m['title'], url=m['url'], tag=m['tag'], state=m['state'])
         db.session.add(movie)
 
     db.session.commit()
